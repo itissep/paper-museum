921bc48ad97cee20addb288914fb8a1b49cd341a
Someone Else's Problem <someonenewish@yandex.ru>
2022-03-02 11:28:19 +0300

    Initial Commit

---
+++ museum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  museum
+//
+//  Created by The GORDEEVS on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
